85e3f7131e1958a33f8fdbdf0ede6292e9c2021f
Ravonne Byrd <rnevels10@gmail.com>
2022-04-21 19:36:52 -0400

    cors for heroku

--- src/main/java/com/example/server/PasswordGeneration/PasswordController.java
+++ src/main/java/com/example/server/PasswordGeneration/PasswordController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "password")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://password-ui.herokuapp.com/"})
 public class PasswordController {
 
     private final PasswordService passwordService;
