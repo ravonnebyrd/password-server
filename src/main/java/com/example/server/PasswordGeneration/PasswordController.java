77845287b7085a350a3fce485c41c293a3d30d11
Ravonne Byrd <rnevels10@gmail.com>
2022-04-21 18:57:07 -0400

    beginning of custom password generation:

--- src/main/java/com/example/server/PasswordGeneration/PasswordController.java
+++ src/main/java/com/example/server/PasswordGeneration/PasswordController.java
@@ -1,11 +1,9 @@
 package com.example.server.PasswordGeneration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "password")
@@ -26,5 +24,25 @@ public class PasswordController {
     @GetMapping
     public ResponseEntity<Password> getRandomPassword(){
         return ResponseEntity.ok().body(passwordService.makeRandomPassword());
+    }
+
+    @PostMapping("/custom")
+    public ResponseEntity<Password> getCustomizedPassword(@RequestBody int length,
+                                                          String symbols,
+                                                          String uppercase,
+                                                          String lowercase,
+                                                          String numbers){
+
+        System.out.println(length + ' ' + symbols + ' ' + uppercase + ' ' + lowercase + ' ' + numbers);
+
+        Password result = passwordService.customizedPassword(length,
+                                    symbols, uppercase, lowercase, numbers);
+
+        if (!result.password.isBlank() && !result.password.isEmpty()){
+            return ResponseEntity.ok().body(result);
+        } else {
+            return ResponseEntity.status(500).body(result);
+        }
+
     }
 }
--- src/main/java/com/example/server/PasswordGeneration/PasswordService.java
+++ src/main/java/com/example/server/PasswordGeneration/PasswordService.java
@@ -2,38 +2,257 @@ package com.example.server.PasswordGeneration;
 
 import org.springframework.stereotype.Service;
 
+import java.util.function.Supplier;
+
 
 @Service
 public class PasswordService {
 
+    // ***** Helper Functions *****
     /*
-        Purpose: To generate a unique length integer, between 8 - 20 inclusive.
+        Purpose: To generate a unique length integer, between 8 - 30 inclusive.
 
         Citation: https://www.educative.io/edpresso/how-to-generate-random-numbers-in-java
         Accessed: 4-20-2022
     */
-    public int randomIntegerBetween8and20(){
+
+    int MIN_ASCII = 33;
+    int MAX_ASCII = 126;
+
+    public int randomIntegerBetween8and30(){
 
         int min = 8;
-        int max = 20;
+        int max = 30;
 
         return (int) Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     /*
-    Purpose: To generate a random ASCII character, between decimal code 33 - 126.
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126 -
+        All.
 
-    Citation: https://beginnersbook.com/2015/05/java-ascii-to-string-conversion/
-    Accessed: 4-20-2022
+        Citation: https://beginnersbook.com/2015/05/java-ascii-to-string-conversion/
+        Accessed: 4-20-2022
     */
     public String randomASCIIBetween33and126(){
 
-        int min = 33;
-        int max = 126;
-
-        int decimalCode = (int) Math.floor(Math.random() * (max - min + 1) + min);
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
         return Character.toString((char) decimalCode);
     }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No lowercase.
+    */
+    public String randomASCIINoLowercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ( decimalCode >= 97 && decimalCode <= 122){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No uppercase.
+    */
+    public String randomASCIINoUppercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ( decimalCode >= 65 && decimalCode <= 90){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No uppercase or lowercase.
+    */
+    public String randomASCIINoUppercaseNoLowercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ( (decimalCode >= 65 && decimalCode <= 90) || ( decimalCode >= 97 && decimalCode <= 122)){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No numbers.
+    */
+    public String randomASCIINoNumbers(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while (decimalCode >= 48 && decimalCode <= 57){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No numbers or lowercase.
+    */
+    public String randomASCIINoNumbersNoLowercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ((decimalCode >= 48 && decimalCode <= 57) || ( decimalCode >= 97 && decimalCode <= 122)){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No numbers or uppercase.
+    */
+    public String randomASCIINoNumbersNoUppercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ((decimalCode >= 48 && decimalCode <= 57) || ( decimalCode >= 65 && decimalCode <= 90)){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No numbers or uppercase or lowercase.
+    */
+    public String randomASCIINoNumbersNoUppercaseNoLowercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ((decimalCode >= 48 && decimalCode <= 57) || ( decimalCode >= 65 && decimalCode <= 90)
+        ||  ( decimalCode >= 97 && decimalCode <= 122)){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No symbols.
+    */
+    public String randomASCIINoSymbols(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while (!((decimalCode >= 48 && decimalCode <= 57) || ( decimalCode >= 65 && decimalCode <= 90)
+                ||  ( decimalCode >= 97 && decimalCode <= 122))){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No symbols or lowercase.
+    */
+    public String randomASCIINoSymbolsNoLowercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while (!((decimalCode >= 48 && decimalCode <= 57) || ( decimalCode >= 65 && decimalCode <= 90))){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No symbols no uppercase.
+    */
+    public String randomASCIINoSymbolsNoUppercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while (!((decimalCode >= 48 && decimalCode <= 57) || ( decimalCode >= 97 && decimalCode <= 122))){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No uppercase no symbols no lowercase.
+    */
+    public String randomASCIINoSymbolsNoUppercaseNoLowercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ( !(decimalCode >= 48 && decimalCode <= 57) ){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+      Purpose: To generate a random ASCII character, between decimal code 33 - 126
+      No symbols no numbers.
+    */
+    public String randomASCIINoSymbolsNoNumbers(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while (!(( decimalCode >= 65 && decimalCode <= 90) ||  ( decimalCode >= 97 && decimalCode <= 122))){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No symbols no numbers no lowercase.
+    */
+    public String randomASCIINoSymbolsNoNumbersNoLowercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ( !(decimalCode >= 65 && decimalCode <= 90)){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    /*
+        Purpose: To generate a random ASCII character, between decimal code 33 - 126
+        No symbols no numbers no uppercase.
+    */
+    public String randomASCIINoSymbolsNoNumbersNoUppercase(){
+
+        int decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+
+        while ( !(decimalCode >= 97 && decimalCode <= 122)){
+            decimalCode = (int) Math.floor(Math.random() * (MAX_ASCII - MIN_ASCII + 1) + MIN_ASCII);
+        }
+
+        return Character.toString((char) decimalCode);
+    }
+
+    // ***** Main Functions *****
 
     /*
         Purpose: To create a unique, auto-generated password.
@@ -44,7 +263,7 @@ public class PasswordService {
     public Password makeRandomPassword(){
 
         // password length
-        int length = randomIntegerBetween8and20();
+        int length = randomIntegerBetween8and30();
 
         // str to return
         StringBuilder str = new StringBuilder();
@@ -58,4 +277,127 @@ public class PasswordService {
 
     }
 
+    /*
+        Purpose: To create a customized password adhering to the presence of the following parameters.
+
+        Citation: https://stackoverflow.com/questions/54303371/can-you-set-a-method-to-a-variable
+        Accessed: 4-21-2022
+    */
+    public Password customizedPassword(int length,
+                                     String symbols,
+                                     String uppercase,
+                                     String lowercase,
+                                     String numbers) {
+
+        int passwordLength = (length != 0) ? length : randomIntegerBetween8and30();
+
+        // str to return
+        StringBuilder str = new StringBuilder();
+
+        // mask the function we will use
+        Supplier<String> passwordFunction = null;
+
+        // 4 options, 16 combinations
+        if ((!symbols.isEmpty() && !symbols.isBlank()) && (!uppercase.isEmpty() && !uppercase.isBlank())
+                && (!lowercase.isEmpty() && !lowercase.isBlank()) && (!numbers.isEmpty() && !numbers.isBlank())){
+
+            // All set
+            passwordFunction = this::randomASCIIBetween33and126;
+
+        } else if ((!symbols.isEmpty() && !symbols.isBlank()) && (!uppercase.isEmpty() && !uppercase.isBlank())
+                && (lowercase.isEmpty() || lowercase.isBlank()) && (!numbers.isEmpty() && !numbers.isBlank())){
+
+            // lowercase is null
+            passwordFunction = this::randomASCIINoLowercase;
+
+        } else if ((!symbols.isEmpty() && !symbols.isBlank()) && (uppercase.isEmpty() || uppercase.isBlank())
+                && (!lowercase.isEmpty() && !lowercase.isBlank()) && (!numbers.isEmpty() && !numbers.isBlank())){
+
+            // uppercase is null
+            passwordFunction = this::randomASCIINoUppercase;
+
+        } else if ((!symbols.isEmpty() && !symbols.isBlank()) && (uppercase.isEmpty() || uppercase.isBlank())
+                && (lowercase.isEmpty() || lowercase.isBlank()) && (!numbers.isEmpty() && !numbers.isBlank())){
+
+            passwordFunction = this::randomASCIINoUppercaseNoLowercase;
+
+        } else if ((!symbols.isEmpty() && !symbols.isBlank()) && (!uppercase.isEmpty() && !uppercase.isBlank())
+                && (!lowercase.isEmpty() && !lowercase.isBlank()) && (numbers.isEmpty() || numbers.isBlank())){
+
+            // numbers is null
+            passwordFunction = this::randomASCIINoNumbers;
+
+        } else if ((!symbols.isEmpty() && !symbols.isBlank()) && (!uppercase.isEmpty() && !uppercase.isBlank())
+                && (lowercase.isEmpty() || lowercase.isBlank()) && (numbers.isEmpty() || numbers.isBlank())){
+
+            // numbers and lowercase is null
+            passwordFunction = this::randomASCIINoNumbersNoLowercase;
+
+        } else if ((!symbols.isEmpty() && !symbols.isBlank()) && (uppercase.isEmpty() || uppercase.isBlank())
+                && (!lowercase.isEmpty() && !lowercase.isBlank()) && (numbers.isEmpty() || numbers.isBlank())){
+
+            // numbers and uppercase is null
+            passwordFunction = this::randomASCIINoNumbersNoUppercase;
+
+        } else if ((!symbols.isEmpty() && !symbols.isBlank()) && (uppercase.isEmpty() || uppercase.isBlank())
+                && (lowercase.isEmpty() || lowercase.isBlank()) && (numbers.isEmpty() || numbers.isBlank())){
+
+            // numbers, uppercase, and lowercase null
+            passwordFunction = this::randomASCIINoNumbersNoUppercaseNoLowercase;
+
+        } else if ((symbols.isEmpty() || symbols.isBlank()) && (!uppercase.isEmpty() && !uppercase.isBlank())
+                && (!lowercase.isEmpty() && !lowercase.isBlank()) && (!numbers.isEmpty() && !numbers.isBlank())){
+
+            // symbols null
+            passwordFunction = this::randomASCIINoSymbols;
+
+        } else if ((symbols.isEmpty() || symbols.isBlank()) && (!uppercase.isEmpty() && !uppercase.isBlank())
+                && (lowercase.isEmpty() || lowercase.isBlank()) && (!numbers.isEmpty() && !numbers.isBlank())){
+
+            // symbols and lowercase null
+            passwordFunction = this::randomASCIINoSymbolsNoLowercase;
+
+        } else if ((symbols.isEmpty() || symbols.isBlank()) && (uppercase.isEmpty() || uppercase.isBlank())
+                && (!lowercase.isEmpty() && !lowercase.isBlank()) && (!numbers.isEmpty() && !numbers.isBlank())){
+
+            // symbols and uppercase null
+            passwordFunction = this::randomASCIINoSymbolsNoUppercase;
+
+        } else if ((symbols.isEmpty() || symbols.isBlank()) && (uppercase.isEmpty() || uppercase.isBlank())
+                && (lowercase.isEmpty() || lowercase.isBlank()) && (!numbers.isEmpty() && !numbers.isBlank())){
+
+            // symbols uppercase and lowercase null
+            passwordFunction = this::randomASCIINoSymbolsNoUppercaseNoLowercase;
+
+        } else if ((symbols.isEmpty() || symbols.isBlank()) && (!uppercase.isEmpty() && !uppercase.isBlank())
+                && (!lowercase.isEmpty() && !lowercase.isBlank()) && (numbers.isEmpty() || numbers.isBlank())){
+
+            // symbols and numbers null
+            passwordFunction = this::randomASCIINoSymbolsNoNumbers;
+
+        } else if ((symbols.isEmpty() || symbols.isBlank()) && (!uppercase.isEmpty() && !uppercase.isBlank())
+                && (lowercase.isEmpty() || lowercase.isBlank()) && (numbers.isEmpty() || numbers.isBlank())){
+
+            // symbols lowercase and numbers null
+            passwordFunction = this::randomASCIINoSymbolsNoNumbersNoLowercase;
+
+        } else if ((symbols.isEmpty() || symbols.isBlank()) && (uppercase.isEmpty() || uppercase.isBlank())
+                && (!lowercase.isEmpty() && !lowercase.isBlank()) && (numbers.isEmpty() || numbers.isBlank())){
+
+            // symbols numbers uppercase null
+            passwordFunction = this::randomASCIINoSymbolsNoNumbersNoUppercase;
+
+        } else if ((symbols.isEmpty() || symbols.isBlank()) && (uppercase.isEmpty() || uppercase.isBlank())
+                && (lowercase.isEmpty() || lowercase.isBlank()) && (numbers.isEmpty() || numbers.isBlank())){
+
+            // All null
+            return new Password("");
+
+        }
+
+        // loop to generate string characters
+        str.append(String.valueOf(passwordFunction).repeat(Math.max(0, passwordLength)));
+
+        return new Password(str.toString());
+    }
 }
